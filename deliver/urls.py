d3e749451bf5299d4ce2b4ac53163c46c87eabe8
Marko Miseljic <marko.miseljic.14@gmail.com>
2022-02-01 16:50:30 +0100

    order details and menu and search menu

--- deliver/urls.py
+++ deliver/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from customer.views import About, Index, Order, OrderConfirmation, OrderPayConfirmation
+from customer.views import About, Index, Order, Menu, MenuSearch, OrderConfirmation, OrderPayConfirmation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +26,8 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
+    path('menu/', Menu.as_view(), name='menu'),
+    path('menu/search/', MenuSearch.as_view(), name='menu-search'),
     path('order/', Order.as_view(), name='order'),
     path('order-confirmation/<int:pk>/', OrderConfirmation.as_view(), name='order-confirmation'),
     path('payment-confirmation/', OrderPayConfirmation.as_view(), name='payment-confirmation'),
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,8 @@
 import imp
 from django.urls import path
-from .views import Dashboard
+from .views import Dashboard, OrderDetails
 
 urlpatterns = [
     path('dashboard', Dashboard.as_view(), name='dashboard'),
+    path('orders/<int:pk>/', OrderDetails.as_view(), name='order-details'),
 ]
