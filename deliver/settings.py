b2605601a830417a768582e319a51755010d67e0
Marko Miseljic <marko.miseljic.14@gmail.com>
2022-02-08 11:06:50 +0100

    a

--- deliver/settings.py
+++ deliver/settings.py
@@ -15,8 +15,6 @@ import os
 import environ
 
 env = environ.Env()
-
-# read th .env file
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0hl@c64sywfjm+z9s%d(a2wc#ei=rvh_%-413$8-v)a-ugrs&q'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
